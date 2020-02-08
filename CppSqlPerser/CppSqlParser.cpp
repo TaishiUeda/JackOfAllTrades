cda76758cf98fd1d124a92b440519a1123136c0b
Taishi Ueda <taishi.ueda@gmail.com>
2020-02-08 22:40:40 +0900

    Create Data class

--- CppSqlPerser/CppSqlParser.cpp
+++ CppSqlPerser/CppSqlParser.cpp
@@ -9,37 +9,258 @@
 
 namespace csp{
 
-    bool Data::get(int32_t& value){}
-    bool Data::get(float& value){}
-    bool Data::get(std::string& value){}
-    bool Data::get(Binary_t& value){}
-    bool Data::set(const int32_t& value){}
-    bool Data::set(const float& value){}
-    bool Data::set(const std::string& value){}
-    bool Data::set(const Binary_t& value){}
+    //##############################################################
+    // Data
+    //---------------------------------------------------------
+    Data::Data(){}
+    //---------------------------------------------------------
+    Data::Data(const bool& is_key=false)
+	:is_key_(is_key){}
+    //---------------------------------------------------------
+    Data::Data(Type_t type, const bool& is_key=false)
+	:is_key_(is_key), type_(type){}
 
+    //---------------------------------------------------------
+    bool Data::get(int8_t& value){
+	if(this->type_ == INT8){
+	    value = data_[0];
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::get(int16_t& value){
+	if(this->type_ == INT16){
+	    for(size_t k=0u; k<data_.size(); ++k){
+		(reinterpret_cast<int8_t*>&value)[k] = data_[k]
+	    }
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::get(int32_t& value){
+	if(this->type_ == INT32){
+	    for(size_t k=0u; k<data_.size(); ++k){
+		(reinterpret_cast<int8_t*>&value)[k] = data_[k]
+	    }
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::get(int64_t& value){
+	if(this->type_ == INT64){
+	    for(size_t k=0u; k<data_.size(); ++k){
+		(reinterpret_cast<int8_t*>&value)[k] = data_[k]
+	    }
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::get(double& value){
+	if(this->type_ == REAL){
+	    for(size_t k=0u; k<data_.size(); ++k){
+		(reinterpret_cast<int8_t*>&value)[k] = data_[k]
+	    }
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::get(std::string& value){
+	if(this->type_ == TEXT){
+	    for(size_t k=0u; k<this->data_.size(); ++k){
+		value[k] = static_cast<char>(this->data[k]);
+	    }
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::get(Binary_t& value){
+	if(this->type_ == BLOB){
+	    value = this->data_;
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+
+    //---------------------------------------------------------
+    void Data::set(const int8_t& value){
+	this->data_.resize(1);
+	this->data_[0] = value;
+	this->type_ = INT8;
+    }
+    //---------------------------------------------------------
+    void Data::set(const int16_t& value){
+	this->data_.resize(2);
+	for(size_t k=0u; k<2u; ++k){
+	    this->data_[k] = (reinterpret_cast<int8_t*>&value)[k];
+	}
+	this->type_ = INT16;
+    }
+    //---------------------------------------------------------
+    void Data::set(const int32_t& value){
+	this->data_.resize(4);
+	for(size_t k=0u; k<4u; ++k){
+	    this->data_[k] = (reinterpret_cast<int8_t*>&value)[k];
+	}
+	this->type_ = INT32;
+    }
+    //---------------------------------------------------------
+    void Data::set(const int64_t& value){
+	this->data_.resize(8u);
+	for(size_t k=0u; k<8u; ++k){
+	    this->data_[k] = (reinterpret_cast<int8_t*>&value)[k];
+	}
+	this->type_ = INT64;
+    }
+    //---------------------------------------------------------
+    void Data::set(const double& value){
+	this->data_.resize(8u);
+	for(size_t k=0u; k<8u; ++k){
+	    this->data_[k] = (reinterpret_cast<int8_t*>&value)[k];
+	}
+	this->type_ = REAL;
+    }
+    //---------------------------------------------------------
+    void Data::set(const std::string& value){
+	this->data_.clear();
+	for(size_t k=0u; k<value.size(); ++k){
+	    this->data_.push_back(static_cast<uint8_t>(value[k]));
+	}
+	this->type_ = TEXT;
+    }
+    //---------------------------------------------------------
+    void Data::set(const Binary_t& value){
+	this->data_ = value;
+	this->type_ = BLOB;
+    }
+    //---------------------------------------------------------
+    bool Data::change(const int8_t& value){
+	if(this->type_ == INT8){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::change(const int16_t& value){
+	if(this->type_ == INT16){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::change(const int32_t& value){
+	if(this->type_ == INT32){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::change(const int64_t& value){
+	if(this->type_ == INT64){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::change(const double& value){
+	if(this->type_ == REAL){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::change(const std::string& value){
+	if(this->type_ == TEXT){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+    //---------------------------------------------------------
+    bool Data::change(const Binary_t& value){
+	if(this->type_ == BLOB){
+	    this->set(value);
+	    return true;
+	}
+	else{
+	    return false;
+	}
+    }
+
+    //---------------------------------------------------------
     std::string Data::str(){
 	switch(type_){
 	    case NONE:
 		return "";
 		break;
-	    case INTEGER:
+	    case INT8:
+		int8_t value = 0;
+		this->get(value);
+		return std::to_string(value);
+		break;
+	    case INT16:
+		int16_t value = 0;
+		this->get(value);
+		return std::to_string(value);
+		break;
+	    case INT32:
 		int32_t value = 0;
+		this->get(value);
+		return std::to_string(value);
+		break;
+	    case INT64:
+		int64_t value = 0;
 		this->get(value);
 		return std::to_string(value);
 		break;
 	    case REAL:
-		float value = 0;
+		double value = 0;
 		this->get(value);
 		return std::to_string(value);
 		break;
 	    case TEXT:
-		int32_t value = 0;
+		std::string value;
 		this->get(value);
 		return std::to_string(value);
 		break;
 	    case BLOB:
-		int32_t value = 0;
+		Binary_t value;
 		this->get(value);
 		return std::to_string(value);
 		break;
--- CppSqlPerser/CppSqlParser.hpp
+++ CppSqlPerser/CppSqlParser.hpp
@@ -1,3 +1,5 @@
+#ifndef CPPSQLPARSER_HPP
+#define CPPSQLPARSER_HPP
 #include <sqlite.h>
 #include <string>
 #include <list>
@@ -5,9 +7,13 @@
 
 namespace csp{
 
+    //! Types of data
     enum Type_t{
 	NONE,
-	INTEGER,
+	INT8,
+	INT16,
+	INT32,
+	INT64,
 	REAL,
 	TEXT,
 	BLOB
@@ -15,25 +21,126 @@ namespace csp{
 
     using Binary_t = std::vector<int8_t>;
 
+    //! Data container class
     class Data{
 	public:
+	    Data();
+	    /*!
+	     * \param[in] is_key If true, this data is to be used as key.
+	     *    Only integer type data can be key.
+	     */
+	    explicit Data(const bool& is_key=false);
+	    /*! Set the data type in initializing.
+	     * \param[in] type Type of data
+	     * \param[in] is_key If true, this data is to be used as key.
+	     *    Only integer type data can be key.
+	     */
+	    Data(const Type_t& type, const bool& is_key=false);
+
+	    /*! Put value as a string. This is convenient when create query. */
 	    std::string str();
+
+	    /*! Get value.
+	     * \param[out] value output value.
+	     * \retval true success
+	     * \retval false type of value and that of Data are different.
+	     */
 	    template<T_OUT>
 		bool get(T_OUT& value);
+	    /*! Set value
+	     * \param[in] value to set.
+	     */
 	    template<T_IN>
-		bool set(const T_IN& value);
+		void set(const T_IN& value);
+	    /*! Change value. If a value of different type from Data, it returns false.
+	     * \param[in] value to set.
+	     * \retval true success
+	     * \retval false type of value and that of Data are different.
+	     */
+	    template<T_IN>
+		bool change(const T_IN& value);
 	private:
+	    bool is_key_{false};
 	    Binary_t data_;
-	    Type_t type_;
+	    Type_t type_{NONE};
     };
 
-    using Column_t = std::map<std::string, Data_t>
+    //! Column type.
+    /*! An example of creating a column.
+     *
+     * ```cpp
+     * //Determine data types
+     * Column_t col = {
+     *     {"ID", Data(INT64, true)},
+     *     {"First name", Data(TEXT)},
+     *     {"Second name", Data(TEXT)},
+     *     {"Age", Data(INT32)},
+     *     {"Height_cm", Data(REAL)},
+     *     {"Weight_kg", Data(REAL)},
+     * };
+     *
+     * // Set values
+     * col["ID"].set(0);
+     * col["First name"].set("Alina");
+     * col["Second name"].set("Rain");
+     * col["Age"].set(28)}]
+     * col["Height_cm"].set(165.7);
+     * col["Weight_kg"].set(49.3);
+     * ```
+     */
+    using Column_t = std::map<std::string, Data>;
+
+    //! Column type. This is a list of column.
+    /*! An example of creating a table.
+     *
+     * ```cpp
+     * Table_t table_1 = {col_1, col_2};
+     * ```
+     *
+     * or,
+     *
+     * ```cpp
+     * Table_t table_1;
+     * table_1.push_back(col_1);
+     * table_1.push_back(col_2);
+     * ```
+     *
+     * In case of creating only header.
+     *
+     * ```
+     *
+     */
     using Table_t  = std::list<Column_t>;
+
+    //! Database type containing multiple tables.
+    /*! An example of creating a table.
+     *
+     * ```cpp
+     * DataBase_t db_1 = {
+     *     {"Table_1", table_1},
+     *     {"Table_2", table_2},
+     * };
+     * ```
+     *
+     * or,
+     *
+     * ```cpp
+     * DataBase_t db_1;
+     * db_1["Table_1"] = table_1;
+     * db_1["Table_2"] = table_2;
+     * ```
+     */
     using DataBase_t  = std::map<std::string, Table_t>;
 
-    class CppSqlPerser{
+    std::string create(const Table_t& obj, std::string& err_msg);
+    std::string update(const Column_t& col, const int64_t& key, std::string& err_msg);
+    std::string update(const Column_t& col, std::string& err_msg);
+    std::string insert(const Column_t& col, std::string& err_msg);
 
+    using ErrList_t = std::list<std::string>;
+    ErrList_t exec(const std::string& query);
+    Table_t select(const std::string& query);
 
     };
-
 }
+#endif
