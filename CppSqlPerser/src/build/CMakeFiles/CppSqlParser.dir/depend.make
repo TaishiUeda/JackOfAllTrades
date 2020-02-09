8b8fae4df27721cc9ce3fb33b307759b71403016
Taishi Ueda <taishi.ueda@gmail.com>
2020-02-09 15:15:56 +0900

    gitignore updated

--- CppSqlPerser/src/build/CMakeFiles/CppSqlParser.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CppSqlParser.
-# This may be replaced when dependencies are built.
--- CppSqlPerser/test/build/CMakeFiles/CppSqlPerserTest-symlink.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../CppSqlPerserTest"
-  "CMakeFiles/CppSqlPerserTest-symlink"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CppSqlPerserTest-symlink.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
